5c213162a81acf859ed00e3ae1f4ad51f7a585d9
AbinavRavi <subramathreya@gmail.com>
2023-03-16 23:05:45 +0100

    added config file

--- training/utils/utils.py
+++ training/utils/utils.py
@@ -2,12 +2,16 @@ import numpy as np
 import torch
 import yaml
 
-def set_seed(seed = 1234):
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    np.random.seed(seed)
+class utils:
+    def __init__(self) -> None:
+        pass
 
-def read_config(file):
-    with open(file,'r') as stream:
-        cxr_config = yaml.full_load(stream)
-    return cxr_config
+    def set_seed(seed = 1234):
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        np.random.seed(seed)
+
+    def read_config(file):
+        with open(file,'r') as stream:
+            cxr_config = yaml.full_load(stream)
+        return cxr_config   
